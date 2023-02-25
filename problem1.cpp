315ee2afcfa19dc795716a6e2835b1b136b97965
GUMMADI SAI KIRAN <83577051+machlimoy@users.noreply.github.com>
2023-02-25 11:16:37 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int x,y,m,z;
+  cout << "enter x,y,m,z values ";
+  cin >> x >> y >> m >> z;
+  float sal = (m*y)/(x*z);
+  cout << sal;
+  return 0;
+}
